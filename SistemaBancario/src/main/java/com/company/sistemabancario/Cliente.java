390c2ff303835b8079bc963747b2f39339e881a6
Igor Matos <igorm17_pvp@hotmail.com>
2025-02-19 21:55:23 -0300

    Cria classes

---
+++ SistemaBancario/src/main/java/com/company/sistemabancario/Cliente.java
@@ -0,0 +1,5 @@
+package com.company.sistemabancario;
+
+public class Cliente {
+    
+}
---
+++ SistemaBancario/src/main/java/com/company/sistemabancario/SistemaBancario.java
@@ -0,0 +1,8 @@
+package com.company.sistemabancario;
+
+public class SistemaBancario {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
