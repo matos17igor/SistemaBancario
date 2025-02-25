7d347e655cd1efd55e1432f2ce8c990a357d8a8f
matos17igor <igorm17_pvp@hotmail.com>
2025-02-25 14:26:32 -0300

    Refatora metodos do gerente

--- SistemaBancario/src/main/java/com/company/sistemabancario/Gerente.java
+++ SistemaBancario/src/main/java/com/company/sistemabancario/Gerente.java
@@ -2,9 +2,13 @@ package com.company.sistemabancario;
 
 import com.company.exception.CPFException;
 import com.company.exception.NameException;
+import java.util.*;
 
 public class Gerente extends Usuario{
 
+    private static List<InvestimentoRendaFixa> investimentosRendaFixa = new ArrayList<>();
+    private static List<InvestimentoRendaVariavel> investimentosRendaVariavel = new ArrayList<>();
+    
     public Gerente(String nome, Endereco logradouro, String nascimento, String cpf, Email email, String senha, String telefone) throws NameException, CPFException {
         super(nome, logradouro, nascimento, cpf, email, senha, telefone);
     }
@@ -18,15 +22,44 @@ public class Gerente extends Usuario{
         System.out.println("4. Avaliação de Crédito");
     }
     
+    public void apoiarMovimentacao(Cliente cliente, String operacao, String senhaConfirmacao){
+        if(!cliente.getSenha().equals(senhaConfirmacao)) {
+            System.out.println("Senha incorreta!");
+            return;
+        }
+        
+        if(operacao.equalsIgnoreCase("transferencia") || operacao.equalsIgnoreCase("saque")){
+            // verificar se o valor da operacao esta acima de 1 milhao
+        } 
+        
+        System.out.println("Apoio concedido para a operacao de " + operacao + "do cliente " + cliente.getName());
+    }
+    
     public void cadastrarInvestimentoRendaFixa(InvestimentoRendaFixa investimento){
+        investimentosRendaFixa.add(investimento);
         System.out.println("Investimento em Renda Fixa cadastrado: " + investimento.getNome());
     }
     
     public void cadastrarInvestimentoRendaVariavel(InvestimentoRendaVariavel investimento){
+        investimentosRendaVariavel.add(investimento);
         System.out.println("Investimento em Renda Variavel cadastrado: " + investimento.getNome());
     }
     
-    public void avaliarCredito(Cliente cliente){
-        System.out.println("Credito avalaido para o cliente " + cliente.getName());
+    public void avaliarCredito(Cliente cliente, String senhaConfirmacao){
+        // esta avaliando somente o saldo disponivel
+        if(!cliente.getSenha().equals(senhaConfirmacao)){
+            System.out.println("Senha incorreta!");
+            return;
+        }
+        
+        double saldo = cliente.getConta().getSaldo();
+        String resultado;
+        if(saldo >= 1000){
+            resultado = "aprovado";
+        } else {
+            resultado = "rejeitado";
+        }
+        System.out.println("Credito " + resultado + " para o cliente " + cliente.getName() + "(Saldo: R$ " +  saldo + ")");
     }
+    
 }
