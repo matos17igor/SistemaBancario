18103b56572296e11801bafe330f169cbd47f1ff
Joao <fernandes.joao@estudante.ufjf.br>
2025-02-27 11:29:36 -0300

    Modifica tela login

--- SistemaBancario/src/main/java/com/company/main/SistemaBancario.java
+++ SistemaBancario/src/main/java/com/company/main/SistemaBancario.java
@@ -6,8 +6,6 @@ public class SistemaBancario {
 
     public static void main(String[] args) {
         TelaLogin tela = new TelaLogin();
-        //tela.desenha();
-        TelaCliente telaCliente = new TelaCliente();
-        telaCliente.desenha();
+        tela.desenha();
     }
 }
--- SistemaBancario/src/main/java/com/company/view/TelaLogin.java
+++ SistemaBancario/src/main/java/com/company/view/TelaLogin.java
@@ -6,42 +6,11 @@ import com.company.controller.*;
 public class TelaLogin {
 
     private JFrame tela;
-    private final int WIDTH = 500;
-    private final int HEIGHT = 200;
-
-    private JTextField tfLogin;
-    private JTextField tfSenha;
-    private JButton btnLogin;
 
     public void desenha() {
-
-        tela = new JFrame("Login");
-        tela.setSize(WIDTH, HEIGHT);
-        tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        tela.setLocationRelativeTo(null);
-        tela.setLayout(null);
+        
+        tela = new JFrame();
         tela.setVisible(true);
-
-        JLabel lblUsuario = new JLabel("Usuário:");
-        lblUsuario.setBounds(20, 20, 80, 25);
-        tela.add(lblUsuario);
-
-        tfLogin = new JTextField();
-        tfLogin.setBounds(100, 20, 160, 25);
-        tela.add(tfLogin);
-
-        JLabel lblSenha = new JLabel("Senha:");
-        lblSenha.setBounds(20, 60, 80, 25);
-        tela.add(lblSenha);
-
-        tfSenha = new JPasswordField();
-        tfSenha.setBounds(100, 60, 160, 25);
-        tela.add(tfSenha);
-
-        btnLogin = new JButton("Entrar");
-        btnLogin.setBounds(100, 100, 100, 30);
-        btnLogin.addActionListener(new AutenticarUsuario(this));
-        tela.add(btnLogin);
     }
     
     public void autenticarUsuario(){
